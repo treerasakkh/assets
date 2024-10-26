d143722c3f07ab564765abb6acffc3943658d86f
Teerasak Khontrong <162951052+treerasakkh@users.noreply.github.com>
2024-10-26 23:19:09 +0700

    show update password alert

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -1,4 +1,8 @@
-<section >
+<section>
+    @if (session('status'))
+        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 mb-3 rounded relative"
+        role="alert">{{ session('status') }}</div>
+    @endif
     <header>
         <h2 class="text-lg font-medium text-gray-900">
             {{ __('เปลี่ยนรหัสผ่าน') }}
@@ -15,19 +19,22 @@
 
         <div>
             <x-input-label for="update_password_current_password" :value="__('รหัสผ่านปัจจบัน')" />
-            <x-text-input id="update_password_current_password" name="current_password" type="password" class="mt-1 block w-full px-2 py-1 border" autocomplete="current-password" />
+            <x-text-input id="update_password_current_password" name="current_password" type="password"
+                class="mt-1 block w-full px-2 py-1 border" autocomplete="current-password" />
             <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
         </div>
 
         <div>
             <x-input-label for="update_password_password" :value="__('รหัสผ่านใหม่')" />
-            <x-text-input id="update_password_password" name="password" type="password" class="mt-1 block w-full px-2 py-1 border" autocomplete="new-password" />
+            <x-text-input id="update_password_password" name="password" type="password"
+                class="mt-1 block w-full px-2 py-1 border" autocomplete="new-password" />
             <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
         </div>
 
         <div>
             <x-input-label for="update_password_password_confirmation" :value="__('ยืนยันรหัสผ่านใหม่')" />
-            <x-text-input id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full px-2 py-1 border" autocomplete="new-password" />
+            <x-text-input id="update_password_password_confirmation" name="password_confirmation" type="password"
+                class="mt-1 block w-full px-2 py-1 border" autocomplete="new-password" />
             <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
         </div>
 
@@ -35,13 +42,8 @@
             <x-primary-button>{{ __('Save') }}</x-primary-button>
 
             @if (session('status') === 'password-updated')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600"
-                >{{ __('Saved.') }}</p>
+                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
+                    class="text-sm text-gray-600">{{ __('Saved.') }}</p>
             @endif
         </div>
     </form>
